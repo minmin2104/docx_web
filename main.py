4cca5322f695ee082b387cf88ceecf9e30cdf70c
minmin2104 <aminkafri0045@gmail.com>
2025-10-26 23:18:21 +0800

    refactor

--- main.py
+++ main.py
@@ -1,18 +1,37 @@
 import zipfile
+import sys
 
 
 class App:
     def __init__(self, path):
         self.path = path
+        self.docx_file = None
+        self.zip_obj = None
+        self.__prepare()
+        self.main()
+
+    def main(self):
+        self.dump_docx_content()
+
+    def __prepare(self):
+        try:
+            self.docx_file = open(self.path, "rb")
+        except OSError as e:
+            print(e, file=sys.stderr)
+            sys.exit(1)
+
+        self.zip_obj = zipfile.ZipFile(self.docx_file)
 
     def dump_docx_content(self):
-        with open(self.path, "rb") as f:
-            zip = zipfile.ZipFile(f)
-            name_list = zip.namelist()
-            for name in name_list:
-                print(name)
+        name_list = self.zip_obj.namelist()
+        for name in name_list:
+            print(name)
+        
+    def clean_up(self):
+        self.docx_file.close()
 
 
 if __name__ == "__main__":
     app = App("./test_file.docx")
-    app.dump_docx_content()
+    app.clean_up()
+
