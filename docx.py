38a6a5ed53b9958181c934e67d4e8480350d2911
minmin2104 <aminkafri0045@gmail.com>
2025-10-28 10:58:50 +0800

    refactor: make docx class

---
+++ docx.py
@@ -0,0 +1,33 @@
+import zipfile
+import os
+
+
+class Docx:
+    def __init__(self, path):
+        self.path = path
+        self.docx_file = None
+        self.zip_obj = None
+        self.__prepare()
+
+    def __prepare(self):
+        if not zipfile.is_zipfile(self.path) and self.path.split(".")[-1] != "docx":
+            raise Exception("File must be .docx file")
+            
+        try:
+            self.docx_file = open(self.path, "rb")
+        except OSError as e:
+            raise OSError(e)
+            
+        self.zip_obj = zipfile.ZipFile(self.docx_file)
+
+    def extract_docx_content(self, dirname):
+        self.zip_obj.extractall(os.path.join(os.getcwd(), dirname))
+
+    def dump_docx_content(self):
+        name_list = self.zip_obj.namelist()
+        for name in name_list:
+            print(name)
+
+    def clean_up(self):
+        self.docx_file.close()
+
--- main.py
+++ main.py
@@ -1,45 +1,27 @@
-import zipfile
-import sys, os
+from docx import Docx
+import sys
 
 
 class App:
     def __init__(self, path):
         self.path = path
-        self.docx_file = None
-        self.zip_obj = None
-        self.__prepare()
-        self.main()
-
-    def main(self):
-        self.dump_docx_content()
-        self.extract_docx_content("extracted")
-
-    def __prepare(self):
-        if not zipfile.is_zipfile(self.path) and self.path.split(".")[-1] != "docx":
-            print("Target file is not a docx file", file=sys.stderr)
-            sys.exit(1)
-            
+        self.docx = None
+        
         try:
-            self.docx_file = open(self.path, "rb")
-        except OSError as e:
+            self.docx = Docx(self.path)
+        except Exception as e:
             print(e, file=sys.stderr)
             sys.exit(1)
+            
+        self.main()
+        self.__clean_up()
 
-        self.zip_obj = zipfile.ZipFile(self.docx_file)
+    def main(self):
+        self.docx.dump_docx_content()
 
-    def extract_docx_content(self, dirname):
-        self.zip_obj.extractall(os.path.join(os.getcwd(), dirname))
-
-    def dump_docx_content(self):
-        name_list = self.zip_obj.namelist()
-        for name in name_list:
-            print(name)
-        
-    def clean_up(self):
-        self.docx_file.close()
-
+    def __clean_up(self):
+        self.docx.clean_up()
 
 if __name__ == "__main__":
     app = App("./test_file.docx")
-    app.clean_up()
 
